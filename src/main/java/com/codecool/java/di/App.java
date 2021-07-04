ebc30ffb2ff09a2d1654ee0bc8189d14564bc9e4
Kamil Grondys <kgrondys@gmail.com>
2021-07-04 09:47:15 +0200

    Daniel's idea for loosing dependency

--- src/main/java/com/codecool/java/di/App.java
+++ src/main/java/com/codecool/java/di/App.java
@@ -3,6 +3,8 @@ package com.codecool.java.di;
 public class App {
     public static void main(String[] args) {
         MyConsumer app = new MyConsumer();
+        EmailService service = new EmailService();
+        app.configure(service);
         app.processMessages("Hi, how are you?", "kamil@codecool.com");
     }
 }
--- src/main/java/com/codecool/java/di/MyConsumer.java
+++ src/main/java/com/codecool/java/di/MyConsumer.java
@@ -1,8 +1,11 @@
 package com.codecool.java.di;
 
 public class MyConsumer {
-    private EmailService email = new EmailService();
+    private EmailService email = null;
 
+    public void configure(EmailService srv){
+        email = srv;
+    }
     public void processMessages(String msg, String receiver){
         //logic for msg and receiver validation
         email.sendEmail(msg, receiver);
