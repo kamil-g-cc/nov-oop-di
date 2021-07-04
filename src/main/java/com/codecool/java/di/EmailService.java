88a6f53d526eb0f1743a4a36bf9ebec3b89446f9
Kamil Grondys <kgrondys@gmail.com>
2021-07-04 10:17:03 +0200

    adding MessageServiceConsumer interface

--- src/main/java/com/codecool/java/di/EmailService.java
+++ src/main/java/com/codecool/java/di/EmailService.java
@@ -4,5 +4,6 @@ public class EmailService implements MessageService{
     @Override
     public void sendMessage(String msg, String receiver) {
         System.out.println("Sending e-mail to "+ receiver+ " with message = "+msg);
+        //przerwa do 10:10 :)
     }
 }
--- src/main/java/com/codecool/java/di/MyConsumer.java
+++ src/main/java/com/codecool/java/di/MyConsumer.java
@@ -1,11 +1,12 @@
 package com.codecool.java.di;
 
-public class MyConsumer {
+public class MyConsumer implements MessageServiceConsumer{
     private MessageService email = null;
 
     public MyConsumer(MessageService srv){
         email = srv;
     }
+    @Override
     public void processMessages(String msg, String receiver){
         //logic for msg and receiver validation
         email.sendMessage(msg, receiver);
